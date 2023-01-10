ef851e9beba7a611dfe83fe09d04a24baacc7e58
Tom <twu@lcs.on.ca>
2023-01-10 11:01:48 -0500

    Initial Commit

---
+++ Mod2 Class1 testing/Mod2_Class1_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod2_Class1_testingApp.swift
+//  Mod2 Class1 testing
+//
+//  Created by Tom Wu on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Mod2_Class1_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
